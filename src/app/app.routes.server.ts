7bcf33289b5a62e8403b109b8c47fe558804f649
Raphael Ragoomundun <rragoomundun@protonmail.com>
2025-08-15 19:30:03 +0400

    Reset password

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client,
   },
   {
+    path: 'auth/password/reset/:resetPasswordToken',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
